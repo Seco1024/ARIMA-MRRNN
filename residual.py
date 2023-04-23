575b0d9a9f83f22f6f97201c453ea13394ced447
Seco1024 <deyenqiu@gmail.com>
2023-04-24 03:34:45 +0800

    fix: output dir

--- residual.py
+++ residual.py
@@ -3,13 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-residual_path='./data/VARMA_ARIMA/residual'
+residual_path='./out/VARMA_ARIMA_residual'
 file_n=0
 for root, dir, files in os.walk(residual_path):
     for file in files:
         exec(f"residual_df{file_n} = pd.read_csv(os.path.join(root, file))")
         file_n+=1
 residuals = [eval(f"residual_df{i}") for i in range(file_n)]
-residuals_mean = pd.DataFrame(pd.concat(residuals).mean(), columns=['RMSE error'])
-residuals_mean = residuals_mean.drop(['Unnamed: 0'])
-residuals_mean.to_csv('./data/VARMA_ARIMA/residual/mean.csv', columns=['RMSE error'])
+residuals_mean = pd.DataFrame(pd.concat(residuals).mean(numeric_only=True), columns=['RMSE error'])
+residuals_mean.to_csv('./out/VARMA_ARIMA_residual/mean.csv', columns=['RMSE error'])
