ee684fc78ff4fcebfa962e4acbfd39b13e9a6419
Deyen Chiu <deyenqiu@gmail.com>
2023-04-24 02:46:18 +0800

    feat: residual.py

--- residual.py
+++ residual.py
@@ -10,3 +10,6 @@ for root, dir, files in os.walk(residual_path):
         exec(f"residual_df{file_n} = pd.read_csv(os.path.join(root, file))")
         file_n+=1
 residuals = [eval(f"residual_df{i}") for i in range(file_n)]
+residuals_mean = pd.DataFrame(pd.concat(residuals).mean(), columns=['RMSE error'])
+residuals_mean = residuals_mean.drop(['Unnamed: 0'])
+residuals_mean.to_csv('./data/VARMA_ARIMA/residual/mean.csv', columns=['RMSE error'])
