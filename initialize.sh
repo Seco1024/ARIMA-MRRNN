43c143659c4a0487fb647fa5d56e28d0d84cfb4e
Seco1024 <deyenqiu@gmail.com>
2023-04-23 13:30:22 +0800

    chore: initialize scipt

---
+++ initialize.sh
@@ -0,0 +1,20 @@
+mkdir ./data
+cd ./data
+mkdir ./raw_data
+mkdir ./preprocssed_data
+mkdir ./VARMA_ARIMA
+mkdir ./plot
+cd ./raw_data
+mkdir Major_Investor
+mkdir Margin_Short_Sell
+mkdir Technical
+cd ../VARMA_ARIMA
+mkdir ./after_ARIMA
+mkdir ./after_VARMA
+mkdir ./error
+cd ../plot
+mkdir ./after_ARIMA
+mkdir ./after_VARMA
+cd ../../
+
+python raw_data_collecting
