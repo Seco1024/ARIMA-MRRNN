30a56a799521a48d6cee3d3a4f95c6303f986a4c
Seco1024 <deyenqiu@gmail.com>
2023-05-01 00:38:47 +0800

    .

--- run_VARMA.sh
+++ run_VARMA.sh
@@ -6,8 +6,9 @@ if [ ${preprocess} == ${TRUE} ];then
     read -p "dropna threshold (default=0.01): " drop_ratio
     read -p "window size (default=100): " window
     read -p "stride size (default=100): " stride
+    read -p "features (0 for default, 1 for filtered): " mode
     echo "執行資料前處理中......"
-    python ./data_preprocessing.py --drop=${drop_ratio} --stride=${stride} --window=${window}
+    python ./data_preprocessing.py --drop=${drop_ratio} --stride=${stride} --window=${window} --mode=${mode}
     echo "完成資料前處理"
 fi
 dataset_n=$(ls ./data/preprocessed_data | wc -l)
