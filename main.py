82a803043ba4517051cff4aa3f4e4de7cdd3ae61
Seco1024 <deyenqiu@gmail.com>
2023-05-10 16:56:53 +0800

    chore: run .sh w/threading

---
+++ main.py
@@ -0,0 +1,14 @@
+import threading
+import subprocess
+
+def run_script(script):
+    subprocess.run(['sh', script])
+
+thread1 = threading.Thread(target=run_script, args=('run_VARMA1.sh',))
+thread2 = threading.Thread(target=run_script, args=('run_VARMA2.sh',))
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
---
+++ run_VARMA2.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sleep 10
+paths=$(ls ./data/preprocessed_data | sort)
+files=()
+for p in $paths; do
+    files+=("$(basename "$p")")
+done
+read -p "test set ratio: " test_ratio
+read -p "window size: " window
+
+cd ./src
+for ((i=989; i>500; i--)); do
+    echo "第${i}筆   進行 VARMA/ARIMA 處理: ${files[$i]}..."
+    python ./VARMA_prediction.py --filename=${files[$i]} --testratio=${test_ratio} --window_size=${window}
+done
+
+python ./residual.py --window=${window}
+echo "VARMA/ARIMA 階段執行完畢"
