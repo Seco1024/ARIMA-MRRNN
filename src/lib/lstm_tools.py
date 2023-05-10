38d67fd52c7ce89a65241198fdab887dfe1f3577
Seco10244 <deyenqiu@gmail.com>
2023-05-11 05:36:52 +0800

    feat: lstm training

--- src/lib/lstm_tools.py
+++ src/lib/lstm_tools.py
@@ -1,13 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, LSTM
 from keras.losses import MeanSquaredError
 from keras.optimizers import Adam
-from keras.regularizers import l1_l2
-from sklearn.metrics import mean_squared_error
+from keras import regularizers, metrics
+from sklearn.preprocessing import MinMaxScaler
+from lib import lstm_tools
 import os
+import datetime
+
+def get_today():
+    today = datetime.date.today()
+    date_obj = datetime.datetime.strptime(today, "%Y-%m-%d")
+    formatted_date = date_obj.strftime("%m%d")
+    return formatted_date
+
+parent_dir = parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir))
+today = get_today()
 
 def build_lstm_model(x_shape, y_shape, neurons, dropout_rate, is_doubled_layer, is_regularized):
     model = Sequential()
@@ -24,12 +35,25 @@ def build_lstm_model(x_shape, y_shape, neurons, dropout_rate, is_doubled_layer, 
     else:
         model.add(Dense(x_shape[1], 'relu'))
     model.add(Dense(y_shape, 'linear'))
-    model.compile(optimizer='adam', loss='mse')
+    
+    adam = Adam(lr=0.001)
+    mse = MeanSquaredError()
+    model.compile(optimizer=adam, loss=mse, metrics=[metrics.MSE, metrics.MAE])
     return model
 
 def visialize_loss_plot(history):
-    plt.plot(history.history['loss'], label='training loss')
-    plt.plot(history.history['val_loss'], label='validation loss')
+    plt.plot(history.history['mean_squared_error'], label='training MSE')
+    plt.plot(history.history['val_mean_squared_error'], label='validation MSE')
     plt.legend()
     plt.xlabel('# epochs')
     plt.ylabel('MSE')
+    plt.savefig(os.path.join(parent_dir, f'out/LSTM_plot/{today}.png'))
+    
+def evaluate_model(model, train_X, train_y, val_X, val_y, test_X, test_y):
+    score_train = model.evaluate(train_X, train_y)
+    score_test = model.evaluate(test_X, test_y)
+    score_val = model.evaluate(val_X, val_y)
+    evaluate_df = pd.DataFrame(np.array([score_train[1], score_val[1], score_test[1], 
+                                score_train[2], score_val[2], score_test[2]]).reshape(-1, 6),
+                      columns=["train_MSE", "val_MSE", "test_MSE", "train_MAE", "val_MAE", "test_MAE"])
+    evaluate_df.to_csv(os.path.join(parent_dir, f'out/LSTM_error/{today}.csv'), index=False)
--- src/lstm.py
+++ src/lstm.py
@@ -1,19 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.layers import LSTM
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, LSTM
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 from keras.losses import MeanSquaredError
 from keras.optimizers import Adam
-from keras import regularizers
+from keras import regularizers, metrics, backend
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
+from lib import lstm_tools
 import os
 
-data = []
+# input
+today = lstm_tools.get_today()
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir))
-files_dir = os.path.join(parent_dir, 'data/VARMA_ARIMA/filtered_after_ARIMA/')
+files_dir = os.path.join(parent_dir, 'data/VARMA_ARIMA/after_ARIMA/')
+data = []
 d = 0
 
 for file in os.listdir(files_dir):
@@ -23,14 +25,14 @@ for file in os.listdir(files_dir):
         dates = df.index
         d = 1
     if np.isnan(df).any().any() or np.isnan(df).any().any() :
-        print(file)
         continue
     data.append(df.reset_index(drop=True))
-
+    
+# preprocessing, reshape
+# scaler = MinMaxScaler()
 X, y, train_X, train_y, val_X, val_y, test_X, test_y = [],[],[],[],[],[],[],[]
 past_n = 5
 future_n = 1
-# scaler = MinMaxScaler()
 
 for pair_corr in data:
     # scaler.fit(pair_corr)
@@ -38,18 +40,26 @@ for pair_corr in data:
     for i in range(past_n, len(pair_corr) - future_n + 1):
         X.append(pair_corr[i - past_n:i])
         y.append(pair_corr[i + future_n - 1:i + future_n]['close'])
-
+        
 X = np.array([X[i].values.tolist() for i in range(len(X))])
 y = np.array([a.values.tolist() for a in y])
 train_X, train_y = X[:int(len(X) * 0.7)], y[:int(len(X) * 0.7)]
 val_X, val_y = X[int(len(X) * 0.7):int(len(X) * (0.7 + 0.15))], y[int(len(X) * 0.7):int(len(X) * (0.7 + 0.15))]
 test_X, test_y = X[int(len(X) * (0.7 + 0.15)):], y[int(len(X) * (0.7 + 0.15)):]
+input_shape = (train_X.shape[1], train_X.shape[2])
 
-model = Sequential()
-model.add(LSTM(64, input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(8, 'relu'))
-model.add(Dense(train_y.shape[1], 'linear'))
+# train
+model = lstm_tools.build_lstm_model(input_shape, 1, 64, 0.5, 0, 1)
+checkpoint_dir = os.path.join(parent_dir, f'models/{today}.h5')
+early_stopping = EarlyStopping(monitor='val_loss', patience=6)
+checkpoint_callback = ModelCheckpoint(checkpoint_dir, monitor='val_loss', verbose=1, 
+                                      mode='min', save_best_only=True)
+lr_reduction = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                              patience=3, min_lr=0.0005)
 
-model.compile(optimizer='adam', loss='mse')
-model.summary()
-history = model.fit(train_X, train_y, epochs=10, batch_size=128, validation_data=(val_X, val_y), verbose=1)
+history = model.fit(train_X, train_y, epochs=100, batch_size=128, validation_data=(val_X, val_y), callbacks=[early_stopping, checkpoint_callback, lr_reduction])
+lstm_tools.visialize_loss_plot(history)
+best_model = load_model(os.path.join(parent_dir, f'models/{today}.h5'))
+lstm_tools.evaluate_model(best_model, train_X, train_y, val_X, val_y, test_X, test_y)
+
+
