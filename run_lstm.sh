1dddc2a46f0cc9d8b124c18a94e4563de966d448
Seco10244 <deyenqiu@gmail.com>
2023-05-24 22:33:07 +0800

    fix

--- run_lstm.sh
+++ run_lstm.sh
@@ -1,4 +1,3 @@
 cd ./src
-
-python ./lstm_train.py --neurons=32 --double_layer=0 --l2=0.002 --dropout=0.2
-python ./lstm_pred.py  --neurons=32 --double_layer=0 --l2=0.002 --dropout=0.2
+python ./lstm_train.py  --double_layer=0 --neurons=32 --dropout=0.5
+python ./lstm_pred.py   --double_layer=0 --neurons=32 --dropout=0.5
