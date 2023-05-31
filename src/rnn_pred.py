8e2bd2c1156fef117815be4cbcf8263fd5582ebc
Seco10244 <deyenqiu@gmail.com>
2023-06-01 03:16:43 +0800

    feat: GRU

--- src/rnn_pred.py
+++ src/rnn_pred.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-from utils import lstm_tools
+from utils import rnn_tools
 import random
 import datetime
 import argparse
@@ -13,17 +13,19 @@ logging.basicConfig(level=logging.CRITICAL)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--cell', default='LSTM')
 parser.add_argument('--neurons', default=32, help="number of neurons")
 parser.add_argument('--double_layer', default=0, help="is double layered")
-parser.add_argument('--dropout', default=0.25, help="Dropout Rate")
+parser.add_argument('--dropout', default=0.5, help="Dropout Rate")
 parser.add_argument('--lr', default=0.0005, help="learning rate")
 parser.add_argument('--lookback', default=14)
 args = parser.parse_args()
     
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir))
-files_dir = os.path.join(parent_dir, f'data/VARMA_ARIMA/after_ARIMA/')
-tmp_dir = os.path.join(parent_dir, 'data/VARMA_ARIMA_prediction/after_ARIMA/')
-today = lstm_tools.get_today()
+files_dir = os.path.join(parent_dir, f'data/VARMA_ARIMA/after_ARIMA_full/')
+tmp_dir = os.path.join(parent_dir, 'data/VARMA_ARIMA_prediction/after_ARIMA_full/')
+pred_target = os.listdir(files_dir)[int(len(os.listdir(files_dir))*0.9):]
+today = rnn_tools.get_today()
 if not os.path.exists(os.path.join(parent_dir, f'models/{str(today)}')):
     today = datetime.datetime.strptime(today, '%m%d') - datetime.timedelta(days=1)
     today = datetime.datetime.strftime(today, '%m%d')
@@ -33,47 +35,47 @@ lookback = int(args.lookback)
 future_n = 1
 
 # load model
-best_lstm_model = lstm_tools.lstm_model(int(args.double_layer), int(args.neurons), float(args.dropout), float(args.lr))
-best_lstm_model.restore(today)
+best_rnn_model = rnn_tools.rnn_model(int(args.double_layer), int(args.neurons), float(args.dropout), float(args.lr), cell=args.cell)
+best_rnn_model.restore(today)
 
-for file in os.listdir(tmp_dir):
+for file in pred_target:
     residual_file_path = os.path.join(files_dir, file)
     residual_df = pd.read_csv(residual_file_path, index_col='date', parse_dates=True)
     residual_df = residual_df.reset_index(drop=True)
-    arima_output_path = os.path.join(parent_dir, f'data/VARMA_ARIMA_prediction/after_ARIMA/{file}')
+    arima_output_path = os.path.join(parent_dir, f'data/VARMA_ARIMA_prediction/after_ARIMA_full/{file}')
     arima_output_df = pd.read_csv(arima_output_path, index_col='date', parse_dates=True)
     arima_output_df = arima_output_df.reset_index(drop=True)
     arima_output_df = arima_output_df['close']
-    original_path = os.path.join(parent_dir, f'data/preprocessed_data/{file}')
+    original_path = os.path.join(parent_dir, f'data/preprocessed_data_full/{file}')
     original_df = pd.read_csv(original_path, index_col='date', parse_dates=True)
     timestamps = original_df.index
     original_df = original_df.reset_index(drop=True)
     original_df = original_df['close']
     original_df = original_df[-len(arima_output_df):]
     
-    residual_X, arima_prediction, original, lstm_prediction = [], [], [], []
+    residual_X, arima_prediction, original, rnn_prediction = [], [], [], []
     for i in range(lookback, len(residual_df) - future_n + 1):
         residual_X.append(residual_df[i - lookback:i])
     residual_X = np.array([residual_X[i].values.tolist() for i in range(len(residual_X))])
     
-    lstm_predictions_df = best_lstm_model.predict(residual_X)
+    rnn_predictions_df = best_rnn_model.predict(residual_X)
     
-    for i in range(len(lstm_predictions_df)):
-        lstm_prediction.append(lstm_predictions_df[i][0])
+    for i in range(len(rnn_predictions_df)):
+        rnn_prediction.append(rnn_predictions_df[i][0])
     
-    arima_prediction = arima_output_df[-len(lstm_prediction):].tolist()
-    original = original_df[-len(lstm_prediction):].tolist()
-    timestamps = timestamps[-len(lstm_prediction):].tolist()
-    hybrid_prediction = [x - y for x, y in zip(arima_prediction, lstm_prediction)]
+    arima_prediction = arima_output_df[-len(rnn_prediction):].tolist()
+    original = original_df[-len(rnn_prediction):].tolist()
+    timestamps = timestamps[-len(rnn_prediction):].tolist()
+    hybrid_prediction = [x - y for x, y in zip(arima_prediction, rnn_prediction)]
     
     arima_prediction_mse.append(mean_squared_error(original, arima_prediction))
     arima_prediction_mae.append(mean_absolute_error(original, arima_prediction))
     hybrid_prediction_mse.append(mean_squared_error(original, hybrid_prediction))
     hybrid_prediction_mae.append(mean_absolute_error(original, hybrid_prediction))
     
-    if random.random() < 0.05:
-        best_lstm_model.visualize_prediction_plot(hybrid_prediction, original, arima_prediction, timestamps, today, file)
+    if file == '3008_3034.csv' or file == '2887_2891.csv':
+        best_rnn_model.visualize_prediction_plot(hybrid_prediction, original, arima_prediction, timestamps, today, file)
 
-df_error = pd.DataFrame(columns=['ARIMA prediction MSE', 'ARIMA prediction MAE', 'ARIMA_LSTM prediction MSE', 'ARIMA_LSTM prediction MAE'])
-df_error.loc[0] = lstm_tools.mean(arima_prediction_mse), lstm_tools.mean(arima_prediction_mae), lstm_tools.mean(hybrid_prediction_mse), lstm_tools.mean(hybrid_prediction_mae)
-df_error.to_csv(os.path.join(parent_dir, f'out/hybrid_model_error/{str(today)}/ARIMA-LSTM_{str(args.double_layer)}_{str(args.neurons)}_{str(args.dropout)}_{str(args.lr)}.csv'))
+df_error = pd.DataFrame(columns=['ARIMA prediction MSE', 'ARIMA prediction MAE', f'ARIMA_{args.cell} prediction MSE', f'ARIMA_{args.cell} prediction MAE'])
+df_error.loc[0] = rnn_tools.mean(arima_prediction_mse), rnn_tools.mean(arima_prediction_mae), rnn_tools.mean(hybrid_prediction_mse), rnn_tools.mean(hybrid_prediction_mae)
+df_error.to_csv(os.path.join(parent_dir, f'out/hybrid_model_error/{str(today)}/ARIMA-{args.cell}_{str(args.double_layer)}_{str(args.neurons)}_{str(args.dropout)}_{str(args.lr)}.csv'))
--- src/rnn_train.py
+++ src/rnn_train.py
@@ -2,41 +2,43 @@ import numpy as np
 import pandas as pd
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 from sklearn.preprocessing import MinMaxScaler
-from utils import lstm_tools
+from utils import rnn_tools
 import argparse
 import logging
 import os
-
 from clearml import Task
-task = Task.init(project_name="ARIMA-MRRNN", task_name="ARIMA-LSTM(data=Technologies Company, window=30, stride=15)")
 
 logging.basicConfig(level=logging.CRITICAL)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', default=200)
+parser.add_argument('--cell', default='LSTM')
+parser.add_argument('--epochs', default=500)
 parser.add_argument('--batch', default=64)
 parser.add_argument('--neurons', default=32, help="number of neurons")
 parser.add_argument('--double_layer', default=0, help="is double layered")
-parser.add_argument('--dropout', default=0.25, help="Dropout Rate")
+parser.add_argument('--dropout', default=0.5, help="Dropout Rate")
 parser.add_argument('--lr', default=0.0005, help="learning rate")
 parser.add_argument('--lookback', default=14)
 args = parser.parse_args()
 
-today = lstm_tools.get_today()
+task = Task.init(project_name="ARIMA-MRRNN", task_name=f"ARIMA-{args.cell}")
+today = rnn_tools.get_today()
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.path.pardir))
-files_dir = os.path.join(parent_dir, f'data/VARMA_ARIMA/after_ARIMA/')
+files_dir = os.path.join(parent_dir, f'data/VARMA_ARIMA/after_ARIMA_full/')
+sorted_dir = os.listdir(files_dir)
+
 if not os.path.exists(os.path.join(parent_dir, f'models/{str(today)}')):
     os.makedirs(os.path.join(parent_dir, f'models/{str(today)}'))
-    os.makedirs(os.path.join(parent_dir, f'out/LSTM_error/{str(today)}'))
-    os.makedirs(os.path.join(parent_dir, f'out/LSTM_plot/{str(today)}'))
+    os.makedirs(os.path.join(parent_dir, f'out/{args.cell}_error/{str(today)}'))
+    os.makedirs(os.path.join(parent_dir, f'out/{args.cell}_plot/{str(today)}'))
     os.makedirs(os.path.join(parent_dir, f'out/hybrid_model_error/{str(today)}'))
     os.makedirs(os.path.join(parent_dir, f'out/hybrid_model_plot/{str(today)}'))
 
 data = []
 d = 0
 
-for file in os.listdir(files_dir):
+for file in sorted_dir:
     file_path = os.path.join(files_dir, file)
     df = pd.read_csv(file_path, index_col='date', parse_dates=True)
     if not d:
@@ -66,15 +68,15 @@ test_X, test_y = X[int(len(X) * (0.7 + 0.15)):], y[int(len(X) * (0.7 + 0.15)):]
 input_shape = (train_X.shape[1], train_X.shape[2])
 
 # train
-model = lstm_tools.lstm_model(int(args.double_layer), int(args.neurons), float(args.dropout), float(args.lr))
-lstm_model = model.build(input_shape, 1)
-checkpoint_dir = os.path.join(parent_dir, f'models/{str(today)}/LSTM_{str(args.double_layer)}_{str(args.neurons)}_{str(args.dropout)}_{str(args.lr)}.h5')
-early_stopping = EarlyStopping(monitor='val_loss', patience=50)
+model = rnn_tools.rnn_model(int(args.double_layer), int(args.neurons), float(args.dropout), float(args.lr), cell=args.cell)
+rnn_model = model.build(input_shape, 1)
+checkpoint_dir = os.path.join(parent_dir, f'models/{str(today)}/{args.cell}_{str(args.double_layer)}_{str(args.neurons)}_{str(args.dropout)}_{str(args.lr)}.h5')
+# early_stopping = EarlyStopping(monitor='val_loss', patience=50)
 checkpoint_callback = ModelCheckpoint(checkpoint_dir, monitor='val_loss', verbose=1, 
                                       mode='min', save_best_only=True)
-lr_reduction = ReduceLROnPlateau(monitor='val_loss', factor=0.7,
-                              patience=10, min_lr=pow(0.7, 5)*float(args.lr))
+lr_reduction = ReduceLROnPlateau(monitor='val_loss', factor=0.8,
+                              patience=12)
 
-history = lstm_model.fit(train_X, train_y, epochs=int(args.epochs), batch_size=int(args.batch), validation_data=(val_X, val_y), callbacks=[early_stopping, checkpoint_callback, lr_reduction])
+history = rnn_model.fit(train_X, train_y, epochs=int(args.epochs), batch_size=int(args.batch), validation_data=(val_X, val_y), callbacks=[checkpoint_callback, lr_reduction])
 model.visualize_loss_plot(history, today)
 model.evaluate_model(train_X, train_y, val_X, val_y, test_X, test_y, today)
