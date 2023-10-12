b24a19c8a56936ebc4cbba9f65272e28a43a6375
Seco10244 <deyenqiu@gmail.com>
2023-10-13 04:06:10 +0800

    update models

--- run_rnn.sh
+++ run_rnn.sh
@@ -1,7 +1,8 @@
 cd ./src
-python ./rnn_train.py  --double_layer=0 --neurons=256 --dropout=0.5 --cell="GRU"
-python ./rnn_pred.py   --double_layer=0 --neurons=256 --dropout=0.5 --cell="GRU"
-# python ./self_Attention_train.py --dropout=0.5
-# python ./self_Attention_pred.py --dropout=0.5
-python ./mrrnn_train.py  --neurons=256 --dropout=0.5 --cell="rnn"
-python ./mrrnn_pred.py   --neurons=256 --dropout=0.5 --cell="rnn"
+# python ./self_Attention_train.py --dropout=0.5 --head_size=32 --lookback=30 --epochs=40
+# python ./self_Attention_pred.py --dropout=0.5 --head_size=32 --lookback=30 
+python ./mrrnn_train.py  --neurons=256 --dropout=0.5 --cell="lstm" --lookback=60 --epochs=200
+python ./rnn_train.py    --neurons=256 --dropout=0.5 --cell="LSTM" --lookback=60 --epochs=200
+python ./mrrnn_train.py  --neurons=256 --dropout=0.5 --cell="gru" --lookback=60 --epochs=200
+python ./rnn_train.py    --neurons=256 --dropout=0.5 --cell="GRU" --lookback=60 --epochs=200
+# python ./mrrnn_train.py  --neurons=256 --dropout=0.5 --cell="rnn" --lookback=15 --epochs=200
